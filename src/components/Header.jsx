343773a073febb0f0abd06849cf91e3eb6a8b158
AbhijitSarker <abhijitsarker03@gmail.com>
2023-08-22 00:28:01 +0600

    header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,11 +12,11 @@ const Header = () => {
     console.log(open);
     return (
 
-        <nav className='navbar-items z-50 rounded-md p-2 flex justify-between items-center shadow-lg container mx-auto mt-3'>
+        <nav className='navbar-items z-10 md:sticky top-3 bg-white rounded-md p-2 flex justify-between items-center shadow-lg container mx-auto mt-3'>
 
-            <img className='h-12 ' src={logo} alt="" />
+            <img className='h-12 z-40' src={logo} alt="" />
 
-            <div onClick={() => setOpen(!open)} className="md:hidden z-50">
+            <div onClick={() => setOpen(!open)} className="md:hidden z-40">
                 <span>
                     {open === true
                         ? <FontAwesomeIcon className='h-8 float-right ' icon={faXmark} />
@@ -25,7 +25,7 @@ const Header = () => {
                 </span>
             </div>
 
-            <div style={{ zIndex: 1 }} className={` grid absolute px-10 pt-24 md:pt-0 md:px-0 gap-3 md:gap-0 md:static md:ml-auto  text-center  md:flex font-bold items-center whitespace-nowrap duration-700 ease-in-out  ${open === true ? 'top-0 left-0 right-0' : '-top-full left-0 right-0'}`}>
+            <div className={`bg-white opacity-80 grid absolute px-10 pt-20 md:pt-0 md:px-0 pb-5 md:pb-0 mt-3 md:mt-0 rounded-md gap-3 md:gap-0 md:static md:ml-auto  text-center  md:flex font-bold items-center whitespace-nowrap duration-700 ease-in-out  ${open === true ? 'top-0 left-0 right-0' : '-top-full left-0 right-0'}`}>
 
                 <Link className='hover:bg-orange-400 px-2 py-2 transition duration-250 ease-out hover:ease-in rounded-md hover:text-white' to='/'><FontAwesomeIcon icon={faHome} /> Home</Link>
                 <Link className='hover:bg-orange-400 px-2 py-2 transition duration-250 ease-out hover:ease-in rounded-md hover:text-white' to='/destinations'> <FontAwesomeIcon icon={faMapPin} /> Destinations</Link>
